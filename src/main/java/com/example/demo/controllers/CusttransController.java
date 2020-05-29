42c7130536f9e86b264b9c120f3dc1a961861967
Estuardo Ramos <efrain.ramos.castro@ericsson.com>
2020-05-29 02:36:13 -0600

    :building_construction: move endpoint reference
    the reference to endpoint is moved to group data related to architecture

--- src/main/java/com/example/demo/controllers/CusttransController.java
+++ src/main/java/com/example/demo/controllers/CusttransController.java
@@ -4,6 +4,7 @@ import com.example.demo.entities.Payload;
 import com.example.demo.services.CusttransService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CusttransController {
     
     @Autowired private CusttransService service;
+    @Value("${demo.activemq.endpoint}") private String endpoint;
 
     @PostMapping
     public void sendPayload(Payload payload) {
-        service.sendToQueue(payload);
+        service.sendToQueue(endpoint, payload);
     }
 }
